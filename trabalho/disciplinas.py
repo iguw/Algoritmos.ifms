68a26733dc65c6d513ad021296eaf4d3f08ee83b
iguw <guilherme.eiei@gmail.com>
2025-01-06 05:44:37 -0400

    Resolvendo conflitos em trabalho/turma.py

--- trabalho/disciplinas.py
+++ trabalho/disciplinas.py
@@ -1,1 +1,7 @@
 disciplinas - []
+
+def cadastrar_disciplinas(self, nome, codigo, carga_horaria, professor=None):
+    self.nome = nome
+    self.codigo = codigo
+    self.carga_horaria = carga_horaria
+    self.professor = professor
--- trabalho/professores.py
+++ trabalho/professores.py
@@ -1,1 +1,11 @@
 professores - []
+
+def cadastrar_professores(self, nome, matricula, data_nascimento, sexo, endereco, telefone, email, disciplina):
+        self.nome = nome
+        self.matricula = matricula
+        self.data_nascimento = data_nascimento
+        self.sexo = sexo
+        self.endereco = endereco
+        self.telefone = telefone
+        self.email = email
+        self.disciplina = disciplina
