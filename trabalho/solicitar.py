79e23035506078f1d8dac9262480f9b893b7038a
iguw <guilherme.eiei@gmail.com>
2025-01-06 05:24:44 -0400

    atualizações

--- trabalho/solicitar.py
+++
@@ -1,7 +0,0 @@
-def obter_entrada(mensagem, condicao=None, mensagem_erro=None):
-    while True:
-        entrada = input(mensagem).strip()
-        if condicao is None or condicao(entrada):
-            return entrada
-        if mensagem_erro:
-            print(mensagem_erro)
